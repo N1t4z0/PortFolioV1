cd302b61eb68efb472b8abd77d973438b92662f8
N1t4z0 <agusdelpercio@gmail.com>
2023-11-30 21:23:01 -0300

    Add initial project files

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import NavBar from './components/navbar'
+import './index.css';
+
+function App() {
+ 
+
+  return (
+    <body className='bg-black'>
+     <NavBar/>
+
+    </body>
+  )
+}
+
+export default App
---
+++ src/components/navbar.jsx
@@ -0,0 +1,17 @@
+
+
+function NavBar() {
+
+    return (
+    <div className="bg-white font-inter text-gray-400 font-medium rounded-t-full rounded-b-full border-t-4 border-b-4 border-white mx-auto max-w-lg py-2">
+        <ul className="flex flex-row place-content-around">
+            <li><a href="#home">Acerca de mí</a></li>
+            <li><a href="#about">Contáctame</a></li>
+            <li><a href="#projects">Descargar CV</a></li>
+        </ul>
+    </div>
+            
+    )
+  }
+  
+  export default NavBar
